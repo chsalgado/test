0bec68ee0411cf18eebe8deae4aae13e6f36426c
Christian Adrian Salgado Pacheco <chrisave4@hotmail.com>
2015-08-13 10:20:05 -0500

    Initial commit

---
+++ Test/Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello world");
+            ReadLine();
+        }
+    }
+}
